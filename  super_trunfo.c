d17dcc917e289fd67426fedd3ef7e49c98c5a438
kkauex <cacauerx22@gmail.com>
2025-04-22 11:39:26 +0000

    atualização

---  super_trunfo.c
+++  super_trunfo.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 
 int main () {
-    // Variaveis carta 1 
+    // Variáveis carta 1
     char estado1;
     char codigo1[4];
     char cidade1[50];
-    int populacao1;
+    unsigned long int populacao1;
     float area1;
     float pib1;
     int pontos1;
 
-    // Variaveis carta2
+    // Variáveis carta 2
     char estado2;
     char codigo2[4];
     char cidade2[50];
-    int populacao2;
+    unsigned long int populacao2;
     float area2;
     float pib2;
     int pontos2;
 
-    // Variaveis para calculo
+    // Variáveis para cálculo
     float densidade1, densidade2;
     float pibPerCapita1, pibPerCapita2;
+    float superPoder1, superPoder2;
 
-    // Leitura dos dados da Carta1
+    // Leitura dos dados da Carta 1
     printf("Cadastro da Carta 1 \n");
 
     printf("Estado: ");
-    scanf(" %c", &estado1); // Espaço antes do %c para consumir newline anterior
+    scanf(" %c", &estado1);
 
     printf("Codigo da carta (ex: A01): ");
     scanf("%s", codigo1);
@@ -36,7 +37,7 @@ int main () {
     scanf("%s", cidade1);
 
     printf("População: ");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
 
     printf("Área (km): ");
     scanf("%f", &area1);
@@ -51,7 +52,7 @@ int main () {
     printf("\nCadastro da Carta 2 \n");
 
     printf("Estado: ");
-    scanf(" %c", &estado2); // Espaço antes do %c para consumir newline anterior
+    scanf(" %c", &estado2);
 
     printf("Codigo da carta (ex: A02): ");
     scanf("%s", codigo2);
@@ -60,7 +61,7 @@ int main () {
     scanf("%s", cidade2);
 
     printf("População: ");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Área (km): ");
     scanf("%f", &area2);
@@ -78,30 +79,46 @@ int main () {
     densidade2 = populacao2 / area2;
     pibPerCapita2 = (pib2 * 1000000000) / populacao2;
 
-    // Exibição dos dados Formatados 
-    printf("\nInformacões das Cartas:\n");
+    superPoder1 = (float)populacao1 + area1 + (pib1 * 1000000000) + pontos1 + pibPerCapita1 + (1 / densidade1);
+    superPoder2 = (float)populacao2 + area2 + (pib2 * 1000000000) + pontos2 + pibPerCapita2 + (1 / densidade2);
+
+    // Exibição dos dados
+    printf("\nInformações das Cartas:\n");
 
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", cidade1);
-    printf("População: %d\n", populacao1);
+    printf("População: %lu\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos1);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
     printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
+    printf("Super Poder: %.2f\n", superPoder1);
 
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", cidade2);
-    printf("População: %d\n", populacao2);
+    printf("População: %lu\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos2);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
     printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
+    printf("Super Poder: %.2f\n", superPoder2);
+
+    // Comparações
+    printf("\nComparação de Cartas:\n");
+    printf("População: Carta 1 venceu (%d)\n", populacao1 > populacao2);
+    printf("Área: Carta 1 venceu (%d)\n", area1 > area2);
+    printf("PIB: Carta 1 venceu (%d)\n", pib1 > pib2);
+    printf("Pontos Turísticos: Carta 1 venceu (%d)\n", pontos1 > pontos2);
+    printf("Densidade Populacional: Carta 1 venceu (%d)\n", densidade1 < densidade2);
+    printf("PIB per Capita: Carta 1 venceu (%d)\n", pibPerCapita1 > pibPerCapita2);
+    printf("Super Poder: Carta 1 venceu (%d)\n", superPoder1 > superPoder2);
 
     return 0;
-} 
+}
+
